2382165929670b86c949900ab0bbdaf6e601aff6
Marina2468 <marina.ablaoui@gmail.com>
2024-05-15 16:31:57 +0200

    remove duplicate video frame and crop directly on initial video instead

--- frontend/crop_script.js
+++ frontend/crop_script.js
@@ -1,25 +1,18 @@
-
 //Get the drawing context
-var canvas = document.getElementById("myExample");
-var ctx = canvas.getContext("2d");
-var canvas1 = document.getElementById("myOutput");
-var ctx1 = canvas1.getContext("2d");
+var video = document.getElementById("video");
+var ctx = video.getContext("2d");
+var cropped = document.getElementById("crop");
+var ctx_cropped = cropped.getContext("2d");
 
-
-const image = new Image();
-image.onload = draw; // Draw when image has loaded
-image.src = "sample.png";
-
-
-canvas.addEventListener("mousedown", onPointerDown);
-canvas.addEventListener("mouseup", onPointerUp);
-canvas.addEventListener("mousemove", onPointerMove);
+video.addEventListener("mousedown", onPointerDown);
+video.addEventListener("mouseup", onPointerUp);
+video.addEventListener("mousemove", onPointerMove);
 
 state = "idle";
-mouseDown = { x: 0, y: 0 };
-var pointTopLeft = { x: 125, y: 125 };
-var pointBottomRight = { x: 400, y: 400 };
+var pointTopLeft = { x: 10, y: 10 };
+var pointBottomRight = { x: Math.max(20, video.width / 4), y: Math.max(20, video.height / 4) };
 var center = { x: (pointTopLeft.x + pointBottomRight.x) / 2, y: (pointTopLeft.y + pointBottomRight.y) / 2 };
+var mouseDown = center;
 
 function getEventLocation(e) {
   return {
@@ -51,9 +44,7 @@ function onPointerDown(e) {
   if (minIndex == 2) state = "dC";
   if (minIndex == 3) state = "dCTR";
   if (minIndex == 4) state = "dCBL";
-
 }
-
 
 // mouse move management with the modification of the points location
 function onPointerMove(e) {
@@ -89,20 +80,28 @@ function onPointerMove(e) {
   draw();
 }
 
+
 function onPointerUp(e) {
   state = "idle";  // update the state with the mouse up
 }
 
 
 function draw() {
-  //Clear the background
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // Access the width and height properties
+  const width = window.current_frame.width;
+  const height = window.current_frame.height;
 
-  //Draw the image
-  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+  // Set video dimensions to match the image
+  video.width = width;
+  video.height = height;
 
-  //Draw the points for the corners of the square
+  // Clear any previous content on the canvas
+  ctx.clearRect(0, 0, video.width, video.height);
+
+  // Draw the image onto the video
+  ctx.drawImage(window.current_frame, 0, 0, width, height);
+
+ //Draw the points for the corners of the square
   ctx.fillStyle = "rgba(100,200,255,1)";
   fillCircle(pointTopLeft, 5);
   fillCircle(pointBottomRight, 5);
@@ -117,23 +116,21 @@ function draw() {
   ctx.rect(pointTopLeft.x, pointTopLeft.y, pointBottomRight.x - pointTopLeft.x, pointBottomRight.y - pointTopLeft.y);
   ctx.stroke();
 
-
-  //Canvas 1
+  /*drawing the lil cropped image on the bottom */
+  //Cropped part
   //Clear the background
-  ctx1.fillStyle = "white";
-  // ctx1.fillRect(0, 0, canvas1.width, canvas1.height);
-  ctx1.fillRect(0, 0, (pointBottomRight.x - pointTopLeft.x), (pointBottomRight.y - pointTopLeft.y));
+  ctx_cropped.fillStyle = "white";
+  // ctx_cropped.fillRect(0, 0, cropped.width, cropped.height);
+  ctx_cropped.fillRect(0, 0, (pointBottomRight.x - pointTopLeft.x), (pointBottomRight.y - pointTopLeft.y));
 
 
-  //Compute the ration between the canvas and the image, the draw image operate in the image coordinate system but draw in the canvas coordinate system
-  var rationX = image.width / canvas.width;
-  var rationY = image.height / canvas.height;
-  canvas1.width = parseInt(pointBottomRight.x - pointTopLeft.x);
-  canvas1.height = parseInt(pointBottomRight.y - pointTopLeft.y);
-  ctx1.drawImage(image, pointTopLeft.x * rationX, pointTopLeft.y * rationY, (pointBottomRight.x - pointTopLeft.x) * rationX, (pointBottomRight.y - pointTopLeft.y) * rationY,
+  //Compute the ratio between the container and the actual image, then draw image operate in the image coordinate system but draw in the video coordinate system
+  var rationX = window.current_frame.width / video.width;
+  var rationY = window.current_frame.height / video.height;
+  cropped.width = parseInt(pointBottomRight.x - pointTopLeft.x);
+  cropped.height = parseInt(pointBottomRight.y - pointTopLeft.y);
+  ctx_cropped.drawImage(window.current_frame, pointTopLeft.x * rationX, pointTopLeft.y * rationY, (pointBottomRight.x - pointTopLeft.x) * rationX, (pointBottomRight.y - pointTopLeft.y) * rationY,
     0, 0, (pointBottomRight.x - pointTopLeft.x), (pointBottomRight.y - pointTopLeft.y));
-  
-
 }
 
 function fillCircle(point, radius) {
@@ -141,164 +138,3 @@ function fillCircle(point, radius) {
   ctx.ellipse(point.x, point.y, radius, radius, 0, 0, 2 * Math.PI);
   ctx.fill();
 }
-
-
-const searchButton = document.getElementById("searchButton");
-    searchButton.addEventListener('click', async () => {
-
-        try {
-            let inputValue = document.getElementById("queryInput").value.trim();
-
-            let url = null;
-            let data_send = null;
-
-            if( inputValue == '' ) {
-              const dataURL = canvas1.toDataURL('image/png');
-              const response = await fetch(`${server_url}/upload_png/`, {method: 'POST', body: JSON.stringify({ image_data: dataURL }), headers: {'Content-Type': 'application/json'}});
-              const body = await response.json();
-
-
-
-
-
-
-              
-              console.log(body);
-
-              d3.select("g.axisy")
-                  .selectAll(".od_item")
-                  .remove();
-
-              scores = body;
-              let min_value = d3.min(body['scores'], d => d[1]);
-              let max_value = d3.max(body['scores'], d => d[1]);
-
-              console.log(min_value, max_value);
-
-              const new_yScale = d3.scaleLinear()
-                  .domain([min_value, max_value]) // Range from 0 to 1
-                  .range([height, 0]); // Actual height of the axis
-
-              const new_yAxis = d3.axisLeft(new_yScale)
-                  .ticks(5);
-
-              const new_xScale = d3.scaleLinear()
-                  .domain([0, body['scores'].length]) // Range from 0 to 100
-                  .range([0, width]); // Actual width of the axis
-
-
-              volatile_xScale = new_xScale;
-              
-              const new_xAxis = d3.axisBottom(new_xScale)        
-                  .ticks(10); // Adjust the number of ticks as needed
-                                  
-              d3.select("g.axis")
-                  .transition()
-                  .duration(2000)
-                  .call(new_xAxis);
-
-              d3.select("g.axisy")
-                  .selectAll(".line")
-                  .remove();
-              
-              d3.select("g.axisy")
-                  .append("path")
-                  .datum(body['scores'])
-                  .transition()
-                  .duration(2000)
-                  .attr("d", d3.line()
-                      .x(function(d) { return new_xScale(d[0]); })
-                      .y(function(d) { return new_yScale(d[1]); })
-                  ).attr("class", "line");
-
-              d3.select("g.axis")
-                  .select('.axisy')   
-                  .transition()
-                  .duration(2000)     
-                  .call(new_yAxis);
-
-
-              console.log(body);
-
-              let which_query = document.getElementById("query-element");
-              which_query.innerHTML = inputValue;
-
-              let image = document.getElementById('imsearch-element');
-              image.src = '';
-              
-              
-
-            } else {
-              const response = await fetch(`${server_url}/search?query=${inputValue}`);
-              const body = await response.json();
-              console.log(body);
-
-              d3.select("g.axisy")
-                  .selectAll(".od_item")
-                  .remove();
-
-              scores = body;
-              let min_value = d3.min(body['scores'], d => d[1]);
-              let max_value = d3.max(body['scores'], d => d[1]);
-
-              console.log(min_value, max_value);
-
-              const new_yScale = d3.scaleLinear()
-                  .domain([min_value, max_value]) // Range from 0 to 1
-                  .range([height, 0]); // Actual height of the axis
-
-              const new_yAxis = d3.axisLeft(new_yScale)
-                  .ticks(5);
-
-              const new_xScale = d3.scaleLinear()
-                  .domain([0, body['scores'].length]) // Range from 0 to 100
-                  .range([0, width]); // Actual width of the axis
-
-
-              volatile_xScale = new_xScale;
-              
-              const new_xAxis = d3.axisBottom(new_xScale)        
-                  .ticks(10); // Adjust the number of ticks as needed
-                                  
-              d3.select("g.axis")
-                  .transition()
-                  .duration(2000)
-                  .call(new_xAxis);
-
-              d3.select("g.axisy")
-                  .selectAll(".line")
-                  .remove();
-              
-              d3.select("g.axisy")
-                  .append("path")
-                  .datum(body['scores'])
-                  .transition()
-                  .duration(2000)
-                  .attr("d", d3.line()
-                      .x(function(d) { return new_xScale(d[0]); })
-                      .y(function(d) { return new_yScale(d[1]); })
-                  ).attr("class", "line");
-
-              d3.select("g.axis")
-                  .select('.axisy')   
-                  .transition()
-                  .duration(2000)     
-                  .call(new_yAxis);
-
-
-              console.log(body);
-
-              let which_query = document.getElementById("query-element");
-              which_query.innerHTML = inputValue;
-
-              let image = document.getElementById('imsearch-element');
-              image.src = '';
-            }
-            
-
-
-        } catch (error) {
-            console.error('Error loading search results:', error);
-        }
-    });
-

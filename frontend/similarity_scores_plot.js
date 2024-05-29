fadb21d7b1a788d8981fbd185dda56b77ba93f3a
Marina2468 <marina.ablaoui@gmail.com>
2024-05-29 14:07:27 +0200

    rename variables to be specific

--- frontend/similarity_scores_plot.js
+++ frontend/similarity_scores_plot.js
@@ -1,5 +1,10 @@
 var scorePlot = document.getElementById("scorePlot");
 
+//offset, to keep space between plot and canvas boundaries
+var scorePlotOffsetLeft = 35;
+var scorePlotOffsetRight = 0;
+var scorePlotOffsetY = 20;
+
 //Plot the score curve
 var plotCurve = (currentIndex) => {
     if (window.scores == null) { return; }
@@ -9,46 +14,42 @@ var plotCurve = (currentIndex) => {
     maxScore = Math.max(...window.scores);
     scaledScores = window.scores.map(score => (score - minScore) / (maxScore - minScore));
 
-    //offset, to keep space between plot and canvas boundaries
-    var offsetLeft = 35;
-    var offsetRight = 0;
-    var offsetY = 20;
-
     //canvas with/length
     var plotWidth = scorePlot.clientWidth;
     var plotHeight = scorePlot.clientHeight;
 
+    console.log(plotHeight, plotWidth);
     //reset the drawing
     var ctx = scorePlot.getContext("2d");
     ctx.clearRect(0, 0, plotWidth, plotHeight);
 
-    plotAxes(offsetLeft, offsetRight, offsetY, scorePlot);
+    plotAxes(scorePlotOffsetLeft, scorePlotOffsetRight, scorePlotOffsetY, scorePlot);
 
     // Draw scale values on y-axis
     for (let i = 0; i < 5; i += 1) {
-        const yPos = plotHeight - offsetY - (i / 5) * (plotHeight - 2 * offsetY);
+        const yPos = plotHeight - scorePlotOffsetY - (i / 5) * (plotHeight - 2 * scorePlotOffsetY);
 
         //Math.trunc(value * 100) / 100 := a precision of 0.01
-        ctx.fillText(Math.trunc(((4 - i) * minScore / 4 + i / 4 * maxScore) * 100) / 100.0, offsetLeft / 4, yPos);
+        ctx.fillText(Math.trunc(((4 - i) * minScore / 4 + i / 4 * maxScore) * 100) / 100.0, scorePlotOffsetLeft / 4, yPos);
     }
 
     //selected points highlight
     for (var i = 0; i < window.selectedPoints.length; ++i) {
-        var x = offsetLeft + (window.selectedPoints[i] / (scaledScores.length - 1)) * (plotWidth - offsetRight - offsetLeft);
+        var x = scorePlotOffsetLeft + (window.selectedPoints[i] / (scaledScores.length - 1)) * (plotWidth - scorePlotOffsetRight - scorePlotOffsetLeft);
         ctx.beginPath();
-        ctx.moveTo(x, offsetY);
-        ctx.lineTo(x, plotHeight - offsetY);
+        ctx.moveTo(x, scorePlotOffsetY);
+        ctx.lineTo(x, plotHeight - scorePlotOffsetY);
         ctx.strokeStyle = "rgba(100,200,255,0.3)";
         ctx.stroke();
     }
 
     //draw the curve
     ctx.beginPath();
-    ctx.moveTo(offsetLeft, plotHeight - scaledScores[0] * (plotHeight - 2 * offsetY));
+    ctx.moveTo(scorePlotOffsetLeft, plotHeight - scaledScores[0] * (plotHeight - 2 * scorePlotOffsetY));
 
     for (var i = 1; i < scaledScores.length; i++) {
-        var x = offsetLeft + (i / (scaledScores.length - 1)) * (plotWidth - offsetRight - offsetLeft);
-        var y = plotHeight - offsetY - scaledScores[i] * (plotHeight - 2 * offsetY);
+        var x = scorePlotOffsetLeft + (i / (scaledScores.length - 1)) * (plotWidth - scorePlotOffsetRight - scorePlotOffsetLeft);
+        var y = plotHeight - scorePlotOffsetY - scaledScores[i] * (plotHeight - 2 * scorePlotOffsetY);
         ctx.lineTo(x, y);
     }
 
@@ -57,5 +58,5 @@ var plotCurve = (currentIndex) => {
     ctx.stroke();
 
     //update vertical line position
-    plotMarker(currentIndex, window.scores.length, offsetLeft, offsetRight, offsetY, scorePlot);
+    plotMarker(currentIndex, window.scores.length, scorePlotOffsetLeft, scorePlotOffsetRight, scorePlotOffsetY, scorePlot);
 };

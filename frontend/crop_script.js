b057812ef59cb4d4446699285065270bd1cfacff
Marina2468 <marina.ablaoui@gmail.com>
2024-08-07 14:36:51 +0200

    fix crop area inconsistency

--- frontend/crop_script.js
+++ frontend/crop_script.js
@@ -1,8 +1,6 @@
 // Get the drawing context
 let video = document.getElementById("video");
-let ctx = video.getContext("2d");
 let cropped = document.getElementById("crop");
-let ctx_cropped = cropped.getContext("2d");
 
 let crop_state = "idle";
 let crop_mouse_down = { x: 0, y: 0 };
@@ -21,9 +19,12 @@ const update_crop_center = () => {
 
 let crop_center = update_crop_center();
 
-video.addEventListener("mousedown", (event) => {
+let crop_selection_mouse_down = (event) => {
     crop_mouse_down = {x: event.offsetX, y: event.offsetY};
     crop_center = update_crop_center(); // Update crop center on mousedown
+
+    console.log(crop_mouse_down,crop_center, crop_bot_right, crop_top_left);
+    console.log(video.width, video.height, video.offsetWidth, video.offsetHeight);
 
     // Get the closest point
     let dp1 = length2(crop_mouse_down, window.crop_top_left);
@@ -42,9 +43,11 @@ video.addEventListener("mousedown", (event) => {
     if (min_index == 2) crop_state = "dc";
     if (min_index == 3) crop_state = "dcTR";
     if (min_index == 4) crop_state = "dcBL";
-});
+};
 
-video.addEventListener("mousemove", (event) => {
+video.addEventListener("mousedown", crop_selection_mouse_down);
+
+let crop_selection_mouse_move = (event) => {
     if (crop_state == "idle") return;
 
     let mouse_pos = {x: event.offsetX, y: event.offsetY};
@@ -105,10 +108,13 @@ video.addEventListener("mousemove", (event) => {
     crop_mouse_down = mouse_pos;
 
     update_video(window.current_frame.src);
-});
+};
+video.addEventListener("mousemove", crop_selection_mouse_move);
 
-video.addEventListener("mouseup", () => { crop_state = "idle"; });
-video.addEventListener("mouseout", () => { crop_state = "idle"; });
+let crop_selection_mouse_up = () => { crop_state = "idle"; }
+
+video.addEventListener("mouseup", crop_selection_mouse_up);
+video.addEventListener("mouseout", crop_selection_mouse_up);
 
 let draw_rectangle = (ctx, p1, p2) => {
     // Draw the points for the corners of the square
@@ -128,7 +134,17 @@ let draw_rectangle = (ctx, p1, p2) => {
 }
 
 let draw_cropped = () => {
-    draw_rectangle(ctx, window.crop_top_left, window.crop_bot_right);
+    let video = document.getElementById("video");
+    let ctx = video.getContext("2d");
+    let ctx_cropped = cropped.getContext("2d");
+
+    let x_draw_ratio = video.width / video.offsetWidth;
+    let y_draw_ratio = video.height / video.offsetHeight;
+
+    const parent_div = cropped.parentNode;
+    const max_width = parent_div.offsetWidth * 0.7;
+    draw_rectangle(ctx, {x: window.crop_top_left.x * x_draw_ratio, y: window.crop_top_left.y * y_draw_ratio}, 
+        {x: window.crop_bot_right.x * x_draw_ratio, y: window.crop_bot_right.y * y_draw_ratio});
 
     // Drawing the cropped image on the bottom
     // Cropped part
@@ -138,17 +154,43 @@ let draw_cropped = () => {
         (window.crop_bot_right.y - window.crop_top_left.y));
 
     // Compute the ratio between the container and the actual image, then draw image operate in the image coordinate system but draw in the video coordinate system
-    let ratio_x = window.current_frame.width / video.width;
-    let ratio_y = window.current_frame.height / video.height;
-    cropped.width = parseInt(window.crop_bot_right.x - window.crop_top_left.x);
-    cropped.height = parseInt(window.crop_bot_right.y - window.crop_top_left.y);
-    ctx_cropped.drawImage(
-        window.current_frame, 
-        window.crop_top_left.x * ratio_x, 
-        window.crop_top_left.y * ratio_y, 
-        (window.crop_bot_right.x - window.crop_top_left.x) * ratio_x, 
-        (window.crop_bot_right.y - window.crop_top_left.y) * ratio_y,
-        0, 0, (window.crop_bot_right.x - window.crop_top_left.x), 
-        (window.crop_bot_right.y - window.crop_top_left.y));
+    const ratio_x = window.current_frame.width / video.offsetWidth;
+    const ratio_y = window.current_frame.height / video.offsetHeight;
+    const crop_width = parseInt(window.crop_bot_right.x - window.crop_top_left.x);
+    const crop_height = parseInt(window.crop_bot_right.y - window.crop_top_left.y);
+    const drawing_width = Math.min(max_width, crop_width);
+    const drawing_height = drawing_width / crop_width * crop_height;
+    cropped.width = drawing_width;
+    cropped.height = drawing_height;
+    ctx_cropped.drawImage(window.current_frame, window.crop_top_left.x * ratio_x, 
+        window.crop_top_left.y * ratio_y, crop_width * ratio_x, crop_height * ratio_y, 0, 0, 
+        drawing_width, drawing_height);
 };
 
+toggle_crop.addEventListener("click", () => {
+    let crop = document.getElementById("crop");
+    let crop_label = document.getElementById("crop_label");
+    if (window.is_crop_visible == false) {
+        //update crop square to be a 10th of the image in the center
+        const video = document.getElementById("video");
+        let width = video.offsetWidth;
+        let height = video.offsetHeight;
+        
+        let step_w = width * 0.05;
+        let step_h = height * 0.05;
+
+        window.crop_top_left = { x: width / 2 - step_w, y: height / 2 - step_h };
+        window.crop_bot_right = { x: width / 2 + step_w, y: height / 2 + step_h };
+
+        window.is_crop_visible = true;
+        crop.style.display = "block";
+        crop_label.style.display = "block";
+        update_video(window.current_frame.src);
+    } 
+    else {
+        window.is_crop_visible = false;
+        crop.style.display = "none";
+        crop_label.style.display = "none";
+        update_video(window.current_frame.src);
+    }
+});
